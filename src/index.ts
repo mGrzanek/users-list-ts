29e6a14524c0aa22bdaa03f810ee63e81c030a35
Monika Grzanek <grzanekmonika@o2.pl>
2025-05-06 14:27:30 +0200

    Add UsersData class

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,11 @@ type InquirerAnswers = {
     action: Action
 }
 
+interface User {
+    name: string,
+    age: number
+}
+
 class Message {
     constructor(public content: string){
     }
@@ -42,6 +47,32 @@ class Message {
         if(variant === consoleOption.Success) consola.success(text);
         else if(variant === consoleOption.Info) consola.info(text);
         else consola.error(text);
+    }
+}
+
+class UsersData {
+    data: User[] = [];
+
+    public showAll(): void {
+        let dataInfo = null;
+        Message.showColorized(consoleOption.Info, "Users data");
+        if(this.data.length > 0) dataInfo = console.table(this.data);
+        else console.log("No data...");
+    }
+    public add(userObj: User): void {
+        if(typeof userObj.age === "number" && typeof userObj.name === "string"
+            && userObj.age > 0 && userObj.name.length > 0){
+                this.data.push(userObj);
+                Message.showColorized(consoleOption.Success, "User has been successfully added!");
+        } else Message.showColorized(consoleOption.Error, "Wrong data!");
+    }
+    public remove(userName: string): void {
+        const userToRemove = this.data.find(user => user.name === userName);
+        if(userToRemove) {
+            const userToRemoveIndex = this.data.indexOf(userToRemove);
+            this.data.splice(userToRemoveIndex, 1);
+            Message.showColorized(consoleOption.Success, "User deleted!");
+        } else Message.showColorized(consoleOption.Error, "User not found...");
     }
 }
 
