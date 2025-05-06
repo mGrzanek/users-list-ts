23414dd56a2482cbfdb8da75bc4fdae1b482f0d0
Monika Grzanek <grzanekmonika@o2.pl>
2025-05-06 14:47:43 +0200

    Update StartApp

--- src/index.ts
+++ src/index.ts
@@ -76,17 +76,57 @@ class UsersData {
     }
 }
 
+const users = new UsersData();
+console.log("\n");
+console.info("???? Welcome to the UsersApp!");
+console.log("====================================");
+Message.showColorized(consoleOption.Info, "Available actions");
+console.log("\n");
+console.log("list – show all users");
+console.log("add – add new user to the list");
+console.log("remove – remove user from the list");
+console.log("quit – quit the app");
+console.log("\n");
+
 const startApp = () => {
-  inquirer.prompt([{
-    name: 'action',
-    type: 'input',
-    message: 'How can I help you?',
-  }]).then(async (answers: { action: 'list' | 'add' | 'remove' | 'quit' }) => {
-    console.log("Chosen action: " + answers.action);
-    startApp();
-    if (answers.action === "quit")
-      return;
-  });
+    inquirer.prompt([{
+      name: 'action',
+      type: 'input',
+      message: 'How can I help you?',
+    }]).then(async (answers: InquirerAnswers) => {
+      switch (answers.action) {
+        case Action.List:
+          users.showAll();
+          break;
+        case Action.Add:
+          const user = await inquirer.prompt([{
+            name: 'name',
+            type: 'input',
+            message: 'Enter name',
+          }, {
+            name: 'age',
+            type: 'number',
+            message: 'Enter age',
+          }]);
+          users.add(user);
+          break;
+        case Action.Remove:
+          const name = await inquirer.prompt([{
+            name: 'name',
+            type: 'input',
+            message: 'Enter name',
+          }]);
+          users.remove(name.name);
+          break;
+        case Action.Quit:
+          Message.showColorized(consoleOption.Info, "Bye bye!");
+          return;
+        default:
+            Message.showColorized(consoleOption.Error, "Command not found... Try again...");
+      }
+  
+      startApp();
+    });
 }
 
 startApp();
